d9fe17ddd54d7748cdf93bc76030075acf2cc935
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-07-22 19:51:57 +0300

    fix: load reliable media visibility overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./essay-motion.css";
+import "./essay-media-runtime.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
