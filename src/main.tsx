755a61b7e12d420856035811c87da44a7493da08
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-07-23 03:04:58 +0300

    Load the ambient performance layer after core styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import "./essay-motion.css";
 import "./essay-media-runtime.css";
 import "./media-stability.css";
+import "./ambient-performance.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
