c259e5be0bbed1d00f92ca9ab5f28b07bd66c4e2
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-07-23 14:40:01 +0300

    Load global audio presentation styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./audio-player.css";
 import "./essay-motion.css";
 import App from "./App";
 
